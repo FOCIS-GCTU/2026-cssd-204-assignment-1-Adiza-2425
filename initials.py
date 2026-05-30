644f3742fe42697a73a3a78bdeba62c70d005a82
Adiza-2425 <2425403141@live.gctu.edu.gh>
2026-05-30 21:19:25 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,40 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+# Name: Adiza Saeed Abbas
+# Student ID: 2425403141
+# Email: 2425403141@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Adiza Saeed Abbas, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # prints a small initials header and then displays the initials in large block letter formats.
 
+    # prints a blank line.
+    print()
+
+    #  prints three periods with my initials in capital letters with no spaces
+    print("...ASA")
+
+    # prints a blank line.
+    print()
+
+    # prints three periods with my initials in large letters.
+    print("...AAAAAAAAAAAA..........SSSSSSSSSS........AAAAAAAAAAAA.....")
+    print("...AA........AA.........SSSSSSSSSSS........AA........AA.....")
+    print("...AA........AA........SSSS................AA........AA.....")
+    print("...AA........AA........SSSS................AA........AA.....")
+    print("...AAAAAAAAAAAA..........SSSSSS............AAAAAAAAAAAA.....")
+    print("...AA........AA...........SSSSSS...........AA........AA.....")
+    print("...AA........AA.............SSSSSS.........AA........AA.....")
+    print("...AA........AA.............SSSSSS.........AA........AA.....")
+    print("...AA........AA...**...SSSSSSSSSSS....**...AA........AA...**")
+    print("...AA........AA...**...SSSSSSSSSS.....**...AA........AA...**")
+
+    # prints a blank line.
+    print()
 
 
 main()
